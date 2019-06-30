1a6e5d2e3122ad6934a1e5cf210f52179a336d77
mmicek02 <micekmichael@gmail.com>
2019-06-30 12:41:17 -0500

    added starting code for HTML, CSS and JS

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function getDogImage() {
+  fetch('https://dog.ceo/api/https://dog.ceo/api/breeds/image/random/3')
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
